8943024001d4957315445be50d5fd47d4a5aedd7
Nethmi De Silva <143713658+NethmiTharushi22@users.noreply.github.com>
2025-04-26 02:32:38 +0530

    Create DeleteConfirmModal.tsx
    created Delete Confirm model

---
+++ src/Pages/DeleteConfirmModal.tsx
@@ -0,0 +1,22 @@
+import './Progress.css';
+
+export default function DeleteConfirmModal({
+    onCancel,
+    onConfirm,
+}: {
+    onCancel: () => void;
+    onConfirm: () => void;
+}) {
+    return (
+        <div className="modal-backdrop">
+            <div className="modal">
+                <h4>Are you sure you want to delete this update?</h4>
+                <div className="modal-actions">
+                    <button className="btn-cancel" onClick={onCancel}>Cancel</button>
+                    <button className="btn-delete" onClick={onConfirm}>Delete</button>
+
+                </div>
+            </div>
+        </div>
+    );
+}
