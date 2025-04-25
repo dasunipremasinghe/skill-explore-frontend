9318dfe6178d2b2ebd54eb9494cf951ecc01e2ae
N.P.Kaushani Dewmini <kaushani48952@gmail.com>
2025-04-25 23:55:33 +0530

    add new files

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { commentService } from "../services/CommentService";
-import '../css/Comment.css'
+import '../CSS/Comment.css'
 
 interface User {
   id: string | number;
---
+++ src/types/jest.d.ts
@@ -0,0 +1,3 @@
+// Type declarations for Jest
+declare function test(name: string, fn: () => void): void;
+declare function expect(value: any): any; 
