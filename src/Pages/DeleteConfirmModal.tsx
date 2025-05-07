4147345a2e467eb86b045a4a32fbd5837e7ca8a3
Nethmi De Silva <143713658+NethmiTharushi22@users.noreply.github.com>
2025-05-07 20:49:18 +0530

    Update DeleteConfirmModal.tsx
    Fix the errors in delete confirmation model

--- src/Pages/DeleteConfirmModal.tsx
+++ src/Pages/DeleteConfirmModal.tsx
@@ -1,22 +1,21 @@
-import './Progress.css';
+import '../CSS/Progress.css';
 
 export default function DeleteConfirmModal({
-    onCancel,
-    onConfirm,
+  onCancel,
+  onConfirm,
 }: {
-    onCancel: () => void;
-    onConfirm: () => void;
+  onCancel: () => void;
+  onConfirm: () => void;
 }) {
-    return (
-        <div className="modal-backdrop">
-            <div className="modal">
-                <h4>Are you sure you want to delete this update?</h4>
-                <div className="modal-actions">
-                    <button className="btn-cancel" onClick={onCancel}>Cancel</button>
-                    <button className="btn-delete" onClick={onConfirm}>Delete</button>
-
-                </div>
-            </div>
+  return (
+    <div className="modal-backdrop">
+      <div className="modal">
+        <h4>Are you sure you want to delete this update?</h4>
+        <div className="modal-actions">
+          <button className="btn-cancel" onClick={onCancel}>Cancel</button>
+          <button className="btn-delete" onClick={onConfirm}>Delete</button>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
